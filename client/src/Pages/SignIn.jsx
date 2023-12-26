9179aa95e341fb9df18728dea37aa6a9ee3e4548
nadeemahmadl1 <nadeemahmadmalik6@gmail.com>
2023-12-26 16:39:39 +0530

    completed Signup and Signin page

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7 font-mono">SignIn</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg"
+          type="email"
+          placeholder="Email"
+          id="email"
+        />
+        <input
+          className="border p-3 rounded-lg"
+          type="password"
+          placeholder="password"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="border p-3 rounded-lg"
+          type="text"
+          placeholder="username"
+          id="username"
+        />
+        <input
+          className="border p-3 rounded-lg"
+          type="email"
+          placeholder="email"
+          id="email"
+        />
+        <input
+          className="border p-3 rounded-lg"
+          type="password"
+          placeholder="password"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p className="text-red-500">Already have an Account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
