fa6fabfe27e6256923b594d9093e357da45a15cb
nadeemahmadl1 <nadeemahmadmalik6@gmail.com>
2024-03-09 19:04:04 +0530

    Complete Profile page UI

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className='text-center'>
+      <img
+        className="mx-auto h-10 w-auto mt-5"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+      <h1 className="mx-auto h-10 w-auto mt-5">Home</h1>
+    </div>
+  );
 }
 
 export default Home
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Profile() {
-  return <div>Profile</div>;
+  const {currentUser} = useSelector((state) => state.user);
+  return (
+    <div className="flex flex-col justify-center max-w-lg mx-auto p-3">
+      <h1 className="text-center text-3xl my-5">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="rounded-full h-14 w-14 m-2 object-cover self-center"
+          src={currentUser.avatar}
+          alt="profile"
+        />
+        <input
+          className="p-3 rounded-lg broder"
+          type="text"
+          placeholder="UserName"
+          id="username"
+        />
+        <input
+          className="p-3 rounded-lg broder"
+          type="text"
+          placeholder="Email"
+          id="email"
+        />
+        <input
+          className="p-3 rounded-lg broder"
+          type="text"
+          placeholder="Password"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-60">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-600 cursor-pointer">Delete Account</span>
+        <span className="text-red-600 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
+ 
